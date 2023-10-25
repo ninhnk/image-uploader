28c0daaef8185e50b21449c01652b875a402746a
ninhnk <ninhnk@icitech.net>
2023-10-25 16:08:11 +0700

    update provider

--- src/Providers/ImageUploaderServiceProvider.php
+++ src/Providers/ImageUploaderServiceProvider.php
@@ -19,7 +19,7 @@ class ImageUploaderServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'demo');
         $this->publishes([
